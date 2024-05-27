456122f6bef114135b68239cd52c456db335b4a6
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-27 18:10:49 +0500

    CONTROLLERS: Add Crud Operations

---
+++ ForexFlow.Api/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using ForexFlow.Api.Models.Foundations.Users;
+using ForexFlow.Api.Services.Foundations.Users;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace ForexFlow.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UserController : RESTFulController
+	{
+		private readonly IUserService userService;
+
+		public UserController(IUserService userService)
+		{
+			this.userService = userService;
+		}
+
+		[HttpPost]
+		public async ValueTask<ActionResult<User>> PostUserAsync(User user)
+		{
+			User postedUser = await this.userService.AddUserAsync(user);
+
+			return Created(postedUser);
+		}
+
+		[HttpGet]
+		public ActionResult<User> GetAllUsers()
+		{
+			IQueryable<User> users = this.userService.SelectAllUsers();
+
+			return Ok(users);
+		}
+
+		[HttpGet("{Id}")]
+		public async ValueTask<ActionResult<User>> GetUserByIdAsync(Guid Id)
+		{
+			User gettingUser = await this.userService.SelectUserById(Id);
+
+			return Ok(gettingUser);
+		}
+
+		[HttpPut]
+		public async ValueTask<ActionResult<User>> PutUserAsync(User user)
+		{
+			User updatedUser = await this.userService.ModifyUserAsync(user);
+
+			return Ok(updatedUser);
+		}
+
+		[HttpDelete]
+		public async ValueTask<ActionResult<User>> DeleteUserAsync(Guid Id)
+		{
+			User removedUser = await this.userService.RemoveUserAsync(Id);
+
+			return Ok(removedUser);
+		}
+	}
+}
--- ForexFlow.Api/Program.cs
+++ ForexFlow.Api/Program.cs
@@ -1,11 +1,19 @@
+using ForexFlow.Api.Brokers.Storages;
+using ForexFlow.Api.Controllers;
+using ForexFlow.Api.Services.Foundations.Users;
+using Microsoft.OpenApi.Models;
+
 internal class Program
 {
-	private static void Main(string[] args)
+	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddControllers();
+		builder.Services.AddDbContext<StorageBroker>();
+		AddBrokers(builder);
 		builder.Services.AddEndpointsApiExplorer();
 		builder.Services.AddSwaggerGen();
-
+		builder.Services.AddTransient<IUserService, UserService>();
 		var app = builder.Build();
 
 		if (app.Environment.IsDevelopment())
@@ -15,7 +23,13 @@ internal class Program
 		}
 
 		app.UseHttpsRedirection();
-
+		app.UseAuthorization();
+		app.MapControllers();
 		app.Run();
+	}
+
+	private static void AddBrokers(WebApplicationBuilder builder)
+	{
+		builder.Services.AddTransient<IStorageBroker, StorageBroker>();
 	}
 }
