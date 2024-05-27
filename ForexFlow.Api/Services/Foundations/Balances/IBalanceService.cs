b2fffb74ef48a79283bb68d2c4fd0e4ad9227bcb
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-27 17:07:53 +0500

    SERVICES: Add Crud Operations

---
+++ ForexFlow.Api/Services/Foundations/Balances/IBalanceService.cs
@@ -0,0 +1,13 @@
+﻿using ForexFlow.Api.Models.Foundations.Balances;
+
+namespace ForexFlow.Api.Services.Foundations.Balances
+{
+	public interface IBalanceService
+	{
+		ValueTask<Balance> AddBalanceAsync(Balance Balance);
+		IQueryable<Balance> SelectAllBalances();
+		ValueTask<Balance> SelectBalanceById(Guid Id);
+		ValueTask<Balance> ModifyBalanceAsync(Balance Balance);
+		ValueTask<Balance> RemoveBalanceAsync(Guid Id);
+	}
+}
---
+++ ForexFlow.Api/Services/Foundations/Users/UserService.cs
@@ -0,0 +1,34 @@
+﻿using ForexFlow.Api.Brokers.Storages;
+using ForexFlow.Api.Models.Foundations.Users;
+
+namespace ForexFlow.Api.Services.Foundations.Users
+{
+	public partial class UserService : IUserService
+	{
+		private readonly IStorageBroker storageBroker;
+
+		public UserService(IStorageBroker storageBroker)
+		{
+			this.storageBroker = storageBroker;
+		}
+
+		public async ValueTask<User> AddUserAsync(User user) =>
+			await this.storageBroker.InsertUserAsync(user);
+
+		public IQueryable<User> SelectAllUsers() =>
+			this.storageBroker.RetrieveAllUsers();
+
+		public async ValueTask<User> SelectUserById(Guid Id) =>
+			await this.storageBroker.RetrieveUserById(Id);
+
+		public async ValueTask<User> ModifyUserAsync(User user) =>
+			await this.storageBroker.UpdateUserAsync(user);
+
+		public async ValueTask<User> RemoveUserAsync(Guid Id)
+		{
+			User gettingUser = await this.storageBroker.RetrieveUserById(Id);
+
+			return await this.storageBroker.DeleteUserAsync(gettingUser);
+		}
+	}
+}
