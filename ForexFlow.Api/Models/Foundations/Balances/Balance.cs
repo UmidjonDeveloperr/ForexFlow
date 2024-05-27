30272412307abf0aad9bc11ce3d65d0ec5765eee
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-05-27 15:55:46 +0500

    DATA: Add Models

---
+++ ForexFlow.Api/Models/Foundations/Balances/Balance.cs
@@ -0,0 +1,11 @@
+﻿namespace ForexFlow.Api.Models.Foundations.Balances
+{
+	public class Balance
+	{
+        public Guid Id { get; set; }
+        public decimal USD { get; set; }
+        public decimal UZS { get; set; }
+        public decimal RUB { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
